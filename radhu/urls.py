67be061564cfca84cf11b8db1c2ab13cc2f6ff00
RishabhTyagii <rishabtyagi55@gmail.com>
2026-07-24 23:14:21 +0530

    PWA2

--- radhu/urls.py
+++ radhu/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from stock import views as stock_views
+from django.http import HttpResponse
+from django.views.decorators.cache import never_cache
+from django.conf import settings
+
+@never_cache
+def service_worker(request):
+    sw_path = settings.BASE_DIR / 'static' / 'service-worker.js'
+    with open(sw_path, 'rb') as f:
+        content = f.read()
+    response = HttpResponse(content, content_type='application/javascript')
+    response['Service-Worker-Allowed'] = '/'
+    return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +24,8 @@ urlpatterns = [
     path('raw-materials/', include('rawmaterials.urls')),
     path('cycletyre/', include('cycletyres.urls')),
     path('tallysync/', include('tallysync.urls')),
+    path('service-worker.js', service_worker, name='service_worker'),
 ]
+
+
+
